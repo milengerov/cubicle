d281f0c00ac5df87ce2289dbb32aba87a95cf8ee
Milen Gerov <milen.gerov@gmail.com>
2021-01-27 15:31:40 +0200

    added view engine and home page

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const config = require("./config/config");
 
 const app = express();
 
+app.engine("hbs", handlebars({
+    extname: "hbs",
+}));
+
+app.set("view engine", "hbs");
+
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
-    res.send("Jubaja");
+    res.render("home", {layout: false});
 })
 
 
